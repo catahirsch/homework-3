b492b1b1854ae3da2f9b2ec44f80a2216f6389c3
catahirsch <chirsch@Udesa.edu.ar>
2025-03-06 11:14:43 -0300

    Ejercicio1

---
+++ Ejercicio1/Ejercicio1.cpp
@@ -0,0 +1,42 @@
+// 1. a. Crear una función que dado un valor entero “n”, positivo y mayor a uno, devuelva
+// una matriz cuadrada con valores como en los siguientes ejemplos para n=2 y n=3:
+
+// b. Proveer un código que imprima el contenido de la matriz, utilizando un único ciclo
+// for, desde el mayor valor al menor, es decir, para el ejemplo de Para 𝑀2:
+
+
+#include <iostream>
+using namespace std;
+
+void matriz_cuadrada(int matriz[100][100], int n){
+    int num = 1; int i; int j;
+    for(i=0; i<n; ++i){
+        for(j=0; j<n; ++j){
+            matriz[i][j] = num++;
+        }
+    }
+}
+
+void imprimir_matriz(int matriz[100][100], int n){
+    int i; int j; int total = n * n;
+    for (int k = total; k >= 1; --k) {
+        i = (k - 1) / n;
+        j = (k - 1) % n;
+        cout << "M[" << i << "][" << j << "] = " << matriz[i][j] << endl;
+    }
+}
+
+int main(){
+    int n; int matriz[100][100];int valor = 1;
+
+    cout << "Ingrese un valor entero n (>1): ";
+    cin >> n;
+
+    matriz_cuadrada(matriz, n);
+    imprimir_matriz(matriz, n);
+    
+    return 0;
+}
+
+// Nota: recuerde que se deben imprimir los índices de la matriz
+
